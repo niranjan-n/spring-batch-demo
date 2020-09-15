2601111b339183e7d2e1a6bf7109a7781733a734
niranjann-evolvus <niranjan.n@evolvussolutions.com>
2020-09-15 18:57:21 +0530

    initial cmmit

--- GITCOMMIT/FileReader/src/main/java/com/example/SpringBatch/model/FileData.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringBatch.model;
-
-import lombok.Data;
-
-@Data
-public class FileData {
-   private String name;
-   private String info;
-}
--- GITCOMMIT/FileReader/src/main/resources/schema-h2.sql
+++
@@ -1,4 +0,0 @@
-create table sample_data (
-   name varchar(100),
-   info varchar(100)
-);
